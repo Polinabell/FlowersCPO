5644772b426ff9fed0da6d4ec991d027e8701f9f
Polinabell <polinabell@example.com>
2025-05-20 06:33:29 +0300

    Создала конфигурацию для подключения к базе данных

---
+++ server/src/config/db.config.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  },
+  test: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    logging: false
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+}; 

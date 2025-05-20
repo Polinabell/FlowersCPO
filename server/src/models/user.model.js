28a2b7cd69d22e822ca332172b59d50ac9005a74
Polinabell <polinabell@example.com>
2025-05-20 06:33:37 +0300

    Сделала модель пользователя

---
+++ server/src/models/user.model.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('user', 'supplier', 'seller', 'admin'),
+      defaultValue: 'user',
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    tableName: 'users',
+    timestamps: true
+  });
+
+  return User;
+}; 
